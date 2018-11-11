ba6b50a58fb4912006a76f826e540353c04b7453
Sukant Hajra <rrudbskr5g@snkmail.com>
2018-11-11 02:15:39 -0600

    bump versions of dependencies and fix breaks

--- config.nix
+++ config.nix
@@ -2,6 +2,6 @@
     ghc.default = "ghc843";
     example-nix.rev = "7d01def31eca58a93195f3083145e09dc7ec5a8f";
     example-nix.sha256 = "15m1plc0s222c3pva1c9pij75gs26glagpb94i6dvsmqv2ynmmgh";
-    nixpkgs.rev = "8070a6333f3fc41ef93c2b0e07f999459615cc8d";
-    nixpkgs.sha256 = "0v6nycl7lzr1kdsy151j10ywhxvlb4dg82h55hpjs1dxjamms9i3";
+    nixpkgs.rev = "32340793aafec24dcef95fee46a21e634dd63457";
+    nixpkgs.sha256 = "1jbbg4iafxyc3haa4dk9lg802vsqblq1b2r3gqmy43s8s47g0xwy";
 }
--- shell.nix
+++ shell.nix
@@ -3,5 +3,7 @@ let config = import ./config.nix; in
 args@{ ghcVersion ? config.ghc.default }:
 
 (import ./default.nix args).env.haskell.withEnvTools (pkgs:
-    [ pkgs.haskellPackages.haskell-ci ]
+    if ghcVersion == "ghc861"
+    then []
+    else [ pkgs.haskellPackages.haskell-ci ]
 )
